ac6bf1cc28deb54856acdb04dddd9fe6d42d649d
piyushyadav0191 <piyushyadav0191@gmail.com>
2022-01-14 19:24:27 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import ParticlesBg from 'particles-bg';
+import NavbarMenu from './Navbar';
+import 'tachyons';
+import Blog from './Blog';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  function App(){
+    return (
+      <div>
+        <div>...</div>
+        <ParticlesBg type="lines" num={1200} color="#0000" bg={true}   />
+        
+        <NavbarMenu />
+          <Blog />
+
+      </div>
+    )}
+  
+
 
 export default App;
